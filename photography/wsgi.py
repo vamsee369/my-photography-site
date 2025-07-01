6be00777acbb3e166a36b2b22154a1847ba6bf9b
Vamsee369 <vamseev213@gmail.com>
2025-07-01 10:11:57 +0530

    Fix DJANGO_SETTINGS_MODULE to match folder rename

--- photography/wsgi.py
+++ photography/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'photogrpahy.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'photography.settings')
 
 application = get_wsgi_application()
